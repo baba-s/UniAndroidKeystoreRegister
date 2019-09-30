e46fbc4edbe6cca3ac6ed0997b120be3cc633ba9
baba_s <shotababa1206@gmail.com>
2019-09-30 15:10:37 +0900

    first commit

---
+++ Editor/AndroidKeystoreRegister.cs
@@ -0,0 +1,120 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace KoganeUnityLib
+{
+	internal static class AndroidKeystoreRegister
+	{
+		private sealed class Data
+		{
+			[field: SerializeField] public string KeyaliasPass { get; set; } = null;
+			[field: SerializeField] public string KeystorePass { get; set; } = null;
+		}
+
+		private const string FILENAME = "AndroidKeystoreRegister.json";
+
+		private static Data m_data;
+
+		private static string Directory => Application.persistentDataPath;
+		private static string Path      => $"{Directory}/{FILENAME}";
+
+		[InitializeOnLoadMethod]
+		private static void Run()
+		{
+			Load();
+
+			PlayerSettings.Android.keyaliasPass = m_data.KeyaliasPass;
+			PlayerSettings.Android.keystorePass = m_data.KeystorePass;
+		}
+
+		[SettingsProvider]
+		public static SettingsProvider CreateSettingsProvider()
+		{
+			var path = "Project/Android Keystore Register";
+			var provider = new SettingsProvider( path )
+			{
+				label               = "Android Keystore Register",
+				activateHandler     = OnActive,
+				deactivateHandler   = OnDeactive,
+				titleBarGuiHandler  = null,
+				guiHandler          = OnGUI,
+				footerBarGuiHandler = null,
+				keywords            = new[] { "Android", "keyaliasPass", "keystorePass" },
+			};
+
+			return provider;
+		}
+
+		private static void OnActive( string searchContext, VisualElement rootElement )
+		{
+			Load();
+		}
+
+		private static void OnGUI( string searchContext )
+		{
+			using ( new EditorGUILayout.HorizontalScope() )
+			{
+				EditorGUILayout.PrefixLabel( "keyaliasPass" );
+				m_data.KeyaliasPass = EditorGUILayout.TextField( m_data.KeyaliasPass );
+			}
+
+			using ( new EditorGUILayout.HorizontalScope() )
+			{
+				EditorGUILayout.PrefixLabel( "keystorePass" );
+				m_data.KeystorePass = EditorGUILayout.TextField( m_data.KeystorePass );
+			}
+
+			using ( new EditorGUILayout.HorizontalScope() )
+			{
+				if ( GUILayout.Button( "Open Settings Folder" ) )
+				{
+					Process.Start( Directory );
+				}
+				
+				if ( GUILayout.Button( "Open Settings File" ) )
+				{
+					Process.Start( Path );
+				}
+
+				if ( GUILayout.Button( "Save" ) )
+				{
+					Save();
+				}
+			}
+		}
+
+		private static void OnDeactive()
+		{
+			Save();
+		}
+
+		private static void Load()
+		{
+			if ( !File.Exists( Path ) )
+			{
+				m_data = new Data();
+				return;
+			}
+
+			var reader = new StreamReader( Path );
+			var json   = reader.ReadToEnd();
+
+			m_data = JsonUtility.FromJson<Data>( json );
+
+			reader.Close();
+		}
+
+		private static void Save()
+		{
+			var writer = new StreamWriter( Path );
+			var json   = JsonUtility.ToJson( m_data );
+
+			writer.Write( json );
+
+			writer.Close();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Uni Android Keystore Register
+
+Unity プロジェクトを開いた時に Android の keystore のパスワードを自動で設定するエディタ拡張
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-android-keystore-register.svg)](https://github.com/baba-s/uni-android-keystore-register/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-android-keystore-register": "https://github.com/baba-s/uni-android-keystore-register.git",
+```
+
+manifest.json に上記の記述を追加します  
